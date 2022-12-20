44cd6145d4cea228f265971861a9367265b6fbb7
HSU SHENG KAI <83535048+kevin0920911@users.noreply.github.com>
2022-12-20 17:22:18 +0800

    完成主視窗

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
+import tkinter 
 import tkinter as tk
 from tkinter import ttk
-
+import tkinter.messagebox
+import os
 # -*- coding: big5 -*-
-# def a object : involece
-class involece:
-    def __init__(self,nsa,ns,nf):
-        self.nsa=nsa
-        self.ns=ns
-        self.nf=nf
-
-n = 0
-def transfer():
-    global n
-    n = 1 if n==0 else n                     
-
 # initialize
 
 window_main = tk.Tk()
 
+def transfer():
+    time=box1.get()
+    moddle=box2.get()
+    if time=="本期" and moddle=="後三碼模式":
+        os.system("python thisEasy.py")
+    elif time=="上一期" and moddle=="後三碼模式":
+        os.system("python nextEasy.py")
+    elif time=="本期" and moddle=="全輸入模式":
+        os.system("python thisAll.py")
+    elif time=="上一期" and moddle=="全輸入模式":
+        os.system("python nextAll.py")
+    else:
+        tkinter.messagebox.showinfo(title="錯誤", message="請輸入完整訊息")
+   
 window_main.title('對發票')
 
 window_main.geometry('500x400')
@@ -29,7 +33,7 @@ window_main.iconphoto(True, tk.PhotoImage(file="img\icon.png"))
 
 tk.Label(window_main, text='請輸入你想要對哪一期的發票',font=('Arial',20,'bold')).pack(anchor=tk.CENTER,padx=20, pady=10)
 
-box1 = ttk.Combobox(window_main,font=('Arial',15), values=['本期','這期'])
+box1 = ttk.Combobox(window_main,font=('Arial',15), values=['本期','上一期'])
 
 box1.pack(anchor=tk.CENTER,padx=20, pady=10)
 
@@ -39,13 +43,9 @@ box2 = ttk.Combobox(window_main,font=('Arial',15),values=['後三碼模式','全
 
 box2.pack(anchor=tk.CENTER,padx=20, pady=10)
 
-subimit = tk.Button(window_main, text='送出',font=('Arial',15,'bold'))     
+subimit = tk.Button(window_main, text='送出',font=('Arial',15,'bold'),command=transfer)     
 
-subimit.pack(anchor=tk.CENTER,padx=20, pady=10,command=transfer)
-if n==0:
-    
-else:
-    
+subimit.pack(anchor=tk.CENTER,padx=20, pady=10)
 
 window_main.mainloop()
 
